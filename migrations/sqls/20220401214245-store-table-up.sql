cd1c46b38d17babdb9ac489f012edf3c87bf6be5
DCS97 <96494751+DCS97@users.noreply.github.com>
2022-04-01 22:55:08 +0100

    code review

--- migrations/sqls/20220401214245-store-table-up.sql
+++ migrations/sqls/20220401214245-store-table-up.sql
@@ -2,21 +2,21 @@ CREATE TYPE status_of_order AS ENUM ('active', 'complete');
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  firstName VARCHAR(20) NOT NULL,
-  lastName VARCHAR(20) NOT NULL,
-  password VARCHAR(200) NOT NULL
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  user_password VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE products(
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
   price NUMERIC NOT NULL,
 );
 
 CREATE TABLE orders(
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users (id),
-  status status_of_order NOT NULL,
+  order_status status_of_order NOT NULL,
 );
 
 CREATE TABLE order_products (
